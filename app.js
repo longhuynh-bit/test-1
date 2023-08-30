a50c9f9cadca1bebd28d88306944c1b778b2b9c5
longGCO <ngoclong3066@gmail.com>
2023-08-30 21:56:34 +0700

    test

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 $(document).ready(function () {
-	$(".sepical-list").slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		dots: false,
-		prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='fa fa-long-arrow-left' aria-hidden='true'></i></button>",
-		nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='fa fa-long-arrow-right' aria-hidden='true'></i></button>",
-		autoplay: true,      
-		autoplaySpeed: 1000,    
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					arrows: false,
-				},
-			},
-		],
-	});
+  $(".sepical-list").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow:
+      "<button type='button' class='slick-prev slick-arrow'><i class='fa fa-long-arrow-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next slick-arrow'><i class='fa fa-long-arrow-right' aria-hidden='true'></i></button>",
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
 
+const mobileMenu = document.querySelector("#mobile-menu");
+
+const navbar_menu = document.querySelector(".navbar-menu");
+mobileMenu.addEventListener("click", () => {
+  navbar_menu.classList.toggle("active");
+});
